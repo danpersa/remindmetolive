4205ce9115c584f540f47ea3c4f61c58cb53b97e
Dan Persa <dan.persa@gmail.com>
2012-10-04 23:56:43 +0300

    fixed some tests

--- app/helpers/change_passwords_helper.rb
+++ app/helpers/change_passwords_helper.rb
@@ -1,4 +1,7 @@
 module ChangePasswordsHelper
+  include UsersHelper
+  include ProfilesHelper
+  include ApplicationHelper
 
   def change_passwords_active_class
     return 'active' if "#{params[:controller].parameterize}_#{params[:action].parameterize}" == 'change_passwords_new'
--- app/models/reset_password.rb
+++ app/models/reset_password.rb
@@ -1,27 +1,2 @@
-require 'validators/email_format_validator'
-
-class ResetPassword
-  include ActiveModel::Validations
-  include ActiveModel::Conversion
-  extend ActiveModel::Naming
-
-  attr_accessor :email
-
-  validates :email, :presence     => true,
-                    :email_format => true
-
-  validate :email_must_belong_to_an_user
-
-  def initialize(attributes = {})
-    self.email = attributes[:email]
-  end
-
-  def email_must_belong_to_an_user
-    errors.add(:email, "cannot be found in the database") if
-      (not self.email.blank?) and User.find_by_email(self.email) == nil
-  end
-
-  def persisted?
-    false
-  end
+class ResetPassword < EdgeAuth::ResetPassword
 end
