6f223e31a6c20214f2d33541e55c2ec82e09d589
Dan Persa <dan.persa@gmail.com>
2013-06-20 17:58:50 +0200

    some more idea form

--- app/forms/idea_form.rb
+++ app/forms/idea_form.rb
@@ -20,50 +20,49 @@ class IdeaForm
 
   validate                    :reminder_on_cannot_be_in_the_past
 
-  attr_accessor :content, :privacy, :repeat, :reminder_on
+  attr_accessor :content, :privacy, :repeat, :reminder_on, :idea_id
 
   def initialize(user)
     @user = user
   end
 
   def submit(params)
-    idea_id = params[:idea_id]
-    if idea_id.nil?
-      idea_attributes = params.slice(:content, :privacy)
-                              .merge(created_by: @user, owned_by: @user)
-      idea = Idea.new(idea_attributes)
-      idea_id = idea.id
-    end 
+    self.privacy = params[:privacy]
+    self.content = params[:content]
+    self.repeat = params[:repeat]
+    self.reminder_on = params[:reminder_on]
+    self.idea_id = params[:idea_id]
 
-    user_idea_attributes = params.slice(:privacy, :reminder_on, :repeat)
-                            .merge(user: @user, idea_id: idea_id)
-
-
-    user_idea = UserIdea.new({:privacy => params[:privacy],
-                              :reminder_date => params[:reminder_date],
-                              :reminder_created_at => Time.now,
-                              :idea => idea_id,
-                              :user => self})
-    if idea.valid? and user_idea.valid?
-      idea.save!
-      user_idea.save!
-      User.user_creates_idea_notification self, idea
+    unless valid?
+      return false
     end
-    return user_idea
 
+    idea = initialize_idea params
 
+    next_reminder = NextReminder.new DateTime.now.utc,
+                                     params[:repeat],
+                                     params[:reminder_on]
 
-    user.attributes = params.slice(:username, :email, :password, :password_confirmation)
-    profile.attributes = params.slice(:twitter_name, :github_name, :bio)
-    self.subscribed = params[:subscribed]
-    if valid?
-      generate_token
-      user.save!
-      profile.save!
-      true
+    user_idea = initalize_user_idea params, idea, next_reminder.date
+
+    idea.save!
+    
+    
+    if next_reminder.date.nil?
+      unless user_idea.new_record?
+        # if next reminder is null and we have a user_idea, we delete it
+        user_idea.destroy!
+      end
+      # if next reminder is null, we don't create a new user idea
     else
-      false
+      if user_idea.new_record?
+        user_idea.save!
+      else
+        user_idea.update_attributes! user_idea_attributes
+      end      
     end
+    User.user_creates_idea_notification @user, idea
+    true
   end
 
 
@@ -73,5 +72,35 @@ class IdeaForm
   
   def self.model_name
     ActiveModel::Name.new(self, nil, "User")
+  end
+
+  private
+
+  def initialize_idea params
+    if self.idea_id.nil?
+      idea_attributes = params.slice(:content, :privacy)
+                              .merge(created_by: @user, owned_by: @user)
+      idea = Idea.new idea_attributes
+      self.idea_id = idea.id
+    else
+      idea = Idea.find_by_id self.idea_id
+    end
+    idea
+  end
+
+  def initalize_user_idea params, idea, next_reminder_date
+    user_idea_attributes = params.slice(:privacy, :reminder_on, :repeat)
+                                 .merge user: @user,
+                                        reminder_date: next_reminder_date,
+                                        idea_id: self.idea_id,
+                                        :reminder_created_at => Time.now
+
+    user_idea = nil                                    
+    if @user.has_idea? idea
+      user_idea = @user.user_idea idea
+    else
+      user_idea = UserIdea.new user_idea_attributes
+    end
+    user_idea
   end
 end
