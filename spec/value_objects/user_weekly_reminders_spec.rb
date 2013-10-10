d659429ec3d1f1a3f45f2651997cf9f409833e36
Dan Persa <dan.persa@gmail.com>
2013-10-10 17:19:44 +0200

    fixed some date related tests

--- spec/value_objects/user_weekly_reminders_spec.rb
+++ spec/value_objects/user_weekly_reminders_spec.rb
@@ -4,18 +4,18 @@ describe UserWeeklyReminders do
 
   before do
     @user = FactoryGirl.create :simple_user
-    @user_idea_current_week1 = FactoryGirl.create :user_idea, user: @user, reminder_date: DateTime.new(2013, 7, 25)
-    @user_idea_current_week2 = FactoryGirl.create :user_idea, user: @user, reminder_date: DateTime.new(2013, 7, 28)
-    @user_idea1 = FactoryGirl.create :user_idea, user: @user, reminder_date: DateTime.new(2013, 7, 29)
-    @user_idea2 = FactoryGirl.create :user_idea, user: @user, reminder_date: DateTime.new(2013, 7, 31)
-    @user_idea3 = FactoryGirl.create :user_idea, user: @user, reminder_date: DateTime.new(2013, 8, 4)
-    @user_idea_two_weeks_ahead1 = FactoryGirl.create :user_idea, user: @user, reminder_date: DateTime.new(2013, 8, 5)
-    @user_idea_two_weeks_ahead2 = FactoryGirl.create :user_idea, user: @user, reminder_date: DateTime.new(2013, 8, 8)
+    @user_idea_current_week1 = FactoryGirl.create :user_idea, user: @user, reminder_date: DateTime.new(2016, 8, 25)
+    @user_idea_current_week2 = FactoryGirl.create :user_idea, user: @user, reminder_date: DateTime.new(2016, 8, 28)
+    @user_idea1 = FactoryGirl.create :user_idea, user: @user, reminder_date: DateTime.new(2016, 8, 29)
+    @user_idea2 = FactoryGirl.create :user_idea, user: @user, reminder_date: DateTime.new(2016, 8, 31)
+    @user_idea3 = FactoryGirl.create :user_idea, user: @user, reminder_date: DateTime.new(2016, 9, 4)
+    @user_idea_two_weeks_ahead1 = FactoryGirl.create :user_idea, user: @user, reminder_date: DateTime.new(2016, 9, 5)
+    @user_idea_two_weeks_ahead2 = FactoryGirl.create :user_idea, user: @user, reminder_date: DateTime.new(2016, 9, 8)
   end
 
   describe '#reminders' do
     before do
-      weekly_user_reminders = UserWeeklyReminders.new @user, DateTime.new(2013, 7, 23)
+      weekly_user_reminders = UserWeeklyReminders.new @user, DateTime.new(2016, 8, 23)
       @user_ideas = weekly_user_reminders.reminders
     end
 
